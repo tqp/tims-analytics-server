74c7bf6e06d4a922a8b039bdf39195b9251cafac
tqp <timothyqpowers@gmail.com>
2020-10-29 22:45:42 -0500

    TQP-10: Google Auth Doesn't Update Metadata

--- src/main/java/com/timsanalytics/auth/authGoogle/dao/GoogleAuthDao.java
+++ src/main/java/com/timsanalytics/auth/authGoogle/dao/GoogleAuthDao.java
@@ -28,26 +28,17 @@ public class GoogleAuthDao {
         query.append("  UPDATE\n");
         query.append("      USER\n");
         query.append("  SET\n");
-        query.append("      USER.USER_PROFILE_PHOTO_URL = ?,\n");                 // 1. PICTURE
+        query.append("      USER.USER_PROFILE_PHOTO_URL = ?,\n");
         query.append("      USER.USER_LAST_LOGIN = NOW(),\n");
-        query.append("      USER.USER_LOGIN_COUNT = \n");
-        query.append("      (\n");
-        query.append("          SELECT\n");
-        query.append("              USER.USER_LOGIN_COUNT\n");
-        query.append("          FROM\n");
-        query.append("              USER\n");
-        query.append("          WHERE\n");
-        query.append("              USER.USER_GUID = ?\n");        // 2. USER_GUID
-        query.append("      ) + 1\n");
+        query.append("      USER.USER_LOGIN_COUNT = USER.USER_LOGIN_COUNT + 1\n");
         query.append("  WHERE\n");
-        query.append("      USER.USER_GUID = ?\n");                // 3. USER_GUID
+        query.append("      USER.USER_GUID = ?\n");
         try {
             this.mySqlAuthJdbcTemplate.update(
                     connection -> {
                         PreparedStatement ps = connection.prepareStatement(query.toString());
                         ps.setString(1, googleUser.getPicture());
                         ps.setString(2, appUser.getUserGuid());
-                        ps.setString(3, appUser.getUserGuid());
                         return ps;
                     }
             );
